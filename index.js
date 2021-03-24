f56baa248d15ef1c08eeecb8321a9059067bc9a8
stellar-function-e8f8 <kwooten1993@gmail.com>
2021-03-24 03:38:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,4 +8,37 @@ describe('index', () => {
   const src = path.resolve(__dirname, '..', 'index.js')
   it('calls console.error("HALP!")', done => {
     const spy = expect.spyOn(console, 'error').andCallThrough()
-    
+    jsdom.env(html, [src], {
+      virtualConsole: jsdom.createVirtualConsole().sendTo(console)
+    }, (err, window) => {
+      expect(spy).toHaveBeenCalled('expected console.error to have been called')
+      console.error.restore()
+      done()
+    })
+  })
+
+  it('calls console.log("What is up!")', done => {
+    const spy = expect.spyOn(console, 'log').andCallThrough()
+
+    jsdom.env(html, [src], {
+      virtualConsole: jsdom.createVirtualConsole().sendTo(console)
+    }, (err, window) => {
+      expect(spy).toHaveBeenCalled('expected console.log to have been called')
+      console.log.restore()
+      done()
+    })
+  })
+
+  it('calls console.warn("be curful!")', done => {
+    const spy = expect.spyOn(console, 'warn').andCallThrough()
+
+    jsdom.env(html, [src], {
+      virtualConsole: jsdom.createVirtualConsole().sendTo(console)
+    }, (err, window) => {
+      expect(spy).toHaveBeenCalled('expected console.warn to have been called')
+      console.warn.restore()
+      done()
+    })
+  })
+})
+
