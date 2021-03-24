2aeaa65a1d8608376a3727a79f232401f88302fd
stellar-function-e8f8 <kwooten1993@gmail.com>
2021-03-24 03:37:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,3 +6,6 @@ const path = require('path')
 describe('index', () => {
   const html = '<div></div>'
   const src = path.resolve(__dirname, '..', 'index.js')
+  it('calls console.error("HALP!")', done => {
+    const spy = expect.spyOn(console, 'error').andCallThrough()
+    
