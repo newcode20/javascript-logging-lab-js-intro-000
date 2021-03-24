0623b75d92a003218c088e0e49c8920ae2bede55
stellar-function-e8f8 <kwooten1993@gmail.com>
2021-03-24 03:37:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,8 @@ const expect = require('expect')
 const fs = require('fs')
 const jsdom = require('jsdom')
 const path = require('path')
+
+describe('index', () => {
+  const html = '<div></div>'
+  const src = path.resolve(__dirname, '..', 'index.js')
+
